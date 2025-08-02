254672881825cc98f934e141e84bd5836fd38862
clmarianodev <clmarianodev@gmail.com>
2025-08-02 11:43:06 +0800

    changes

--- index.php
+++ index.php
@@ -2,8 +2,16 @@
 <html lang="en">
 
 <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE+edge">
 
 
 </head>
+
+<body>
+    <div class="container">
+
+    </div>
+</body>
 
 </html>
